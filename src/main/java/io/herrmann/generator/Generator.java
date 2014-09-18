1169a0723324b318b15128a1108fe300cdc2459d
Michael Herrmann <michael.herrmann@bugfree-software.com>
2014-09-18 16:51:01 +0300

    Initial import of Java sources and a bare-bone Maven pom.xml.

---
+++ src/main/java/io/herrmann/generator/Generator.java
@@ -0,0 +1,123 @@
+package io.herrmann.generator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * This class allows specifying Python generator-like sequences. For examples,
+ * see the JUnit test case.
+ *
+ * The implementation uses a separate Thread to produce the sequence items. This
+ * is certainly not as fast as eg. a for-loop, but not horribly slow either. On
+ * a machine with a dual core i5 CPU @ 2.67 GHz, 1000 items can be produced in
+ * < 0.03s.
+ *
+ * By overriding finalize(), the class takes care not to leave any Threads
+ * running longer than necessary.
+ */
+public abstract class Generator<T> implements Iterable<T> {
+
+	private class Condition {
+		private boolean isSet;
+		public synchronized void set() {
+			isSet = true;
+			notify();
+		}
+		public synchronized void await() throws InterruptedException {
+			try {
+				if (isSet)
+					return;
+				wait();
+			} finally {
+				isSet = false;
+			}
+		}
+	}
+
+	static ThreadGroup THREAD_GROUP;
+
+	Thread producer;
+	private boolean hasFinished;
+	private final Condition itemAvailableOrHasFinished = new Condition();
+	private final Condition itemRequested = new Condition();
+	private T nextItem;
+	private boolean nextItemAvailable;
+	private RuntimeException exceptionRaisedByProducer;
+
+	@Override
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+			@Override
+			public boolean hasNext() {
+				waitForNext();
+				return !hasFinished;
+			}
+			@Override
+			public T next() {
+				waitForNext();
+				if (hasFinished)
+					throw new NoSuchElementException();
+				nextItemAvailable = false;
+				return nextItem;
+			}
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+			private void waitForNext() {
+				if (nextItemAvailable || hasFinished)
+					return;
+				if (producer == null)
+					startProducer();
+				itemRequested.set();
+				try {
+					itemAvailableOrHasFinished.await();
+				} catch (InterruptedException e) {
+					hasFinished = true;
+				}
+				if (exceptionRaisedByProducer != null)
+					throw exceptionRaisedByProducer;
+			}
+		};
+	}
+
+	protected abstract void run() throws InterruptedException;
+
+	protected void yield(T element) throws InterruptedException {
+		nextItem = element;
+		nextItemAvailable = true;
+		itemAvailableOrHasFinished.set();
+		itemRequested.await();
+	}
+
+	private void startProducer() {
+		assert producer == null;
+		if (THREAD_GROUP == null)
+			THREAD_GROUP = new ThreadGroup("generatorfunctions");
+		producer = new Thread(THREAD_GROUP, new Runnable() {
+			@Override
+			public void run() {
+				try {
+					itemRequested.await();
+					Generator.this.run();
+				} catch (InterruptedException e) {
+					// No need to do anything here; Remaining steps in run()
+					// will cleanly shut down the thread.
+				} catch (RuntimeException e) {
+					exceptionRaisedByProducer = e;
+				}
+				hasFinished = true;
+				itemAvailableOrHasFinished.set();
+			}
+		});
+		producer.setDaemon(true);
+		producer.start();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		producer.interrupt();
+		producer.join();
+		super.finalize();
+	}
+}
---
+++ src/test/java/io/herrmann/generator/GeneratorTest.java
@@ -0,0 +1,100 @@
+package io.herrmann.generator;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GeneratorTest {
+	@Test
+	public void testEmptyGenerator() {
+		assertEquals(new ArrayList<Object>(), list(new EmptyGenerator()));
+	}
+	private class EmptyGenerator extends Generator {
+		@Override
+		protected void run() {
+		}
+	}
+	public static <T> List<T> list(Iterable<T> iterable) {
+		List<T> result = new ArrayList<T>();
+		for (T item : iterable)
+			result.add(item);
+		return result;
+	}
+	@Test
+	public void testOneEltGenerator() {
+		List<Integer> oneEltList = Arrays.asList(1);
+		assertEquals(oneEltList, list(new ListGenerator<Integer>(oneEltList)));
+	}
+	private class ListGenerator<T> extends Generator<T> {
+		private final List<T> elements;
+		public ListGenerator(List<T> elements) {
+			this.elements = elements;
+		}
+		protected void run() throws InterruptedException {
+			for (T element : elements)
+				yield(element);
+		}
+	}
+	@Test
+	public void testTwoEltGenerator() {
+		List<Integer> twoEltList = Arrays.asList(1, 2);
+		assertEquals(twoEltList, list(new ListGenerator<Integer>(twoEltList)));
+	}
+	@Test
+	public void testInfiniteGenerator() {
+		InfiniteGenerator generator = new InfiniteGenerator();
+		testInfiniteGenerator(generator);
+	}
+	public void testInfiniteGenerator(InfiniteGenerator generator) {
+		int NUM_ELTS_TO_INSPECT = 1000;
+		Iterator<Integer> generatorIterator = generator.iterator();
+		for (int i=0; i < NUM_ELTS_TO_INSPECT; i++) {
+			assertTrue(generatorIterator.hasNext());
+			assertEquals(1, (int) generatorIterator.next());
+		}
+	}
+	private class InfiniteGenerator extends Generator<Integer> {
+		@Override
+		protected void run() throws InterruptedException {
+			while (true)
+				yield(1);
+		}
+	}
+	@Test
+	public void testInfiniteGeneratorLeavesNoRunningThreads() throws Throwable {
+		InfiniteGenerator generator = new InfiniteGenerator();
+		testInfiniteGenerator(generator);
+		generator.finalize();
+		assertEquals(Thread.State.TERMINATED, generator.producer.getState());
+	}
+
+	private class CustomRuntimeException extends RuntimeException {}
+
+	private class GeneratorRaisingException extends Generator<Integer> {
+		@Override
+		protected void run() throws InterruptedException {
+			throw new CustomRuntimeException();
+		}
+	}
+
+	@Test(expected = CustomRuntimeException.class)
+	public void testGeneratorRaisingExceptionHasNext() {
+		GeneratorRaisingException generator = new GeneratorRaisingException();
+		Iterator<Integer> iterator = generator.iterator();
+		iterator.hasNext();
+	}
+
+	@Test(expected = CustomRuntimeException.class)
+	public void testGeneratorRaisingExceptionNext() {
+		GeneratorRaisingException generator = new GeneratorRaisingException();
+		Iterator<Integer> iterator = generator.iterator();
+		iterator.next();
+	}
+
+}
